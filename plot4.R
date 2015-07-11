24ebd70ae76cece2b93156fa5c327db5bd978784
Brennan Cleveland <warriorspot@gmail.com>
2015-07-11 13:12:09 -0500

    Create additional source files

---
+++ plot4.R
@@ -0,0 +1,31 @@
+
+fileName = "household_power_consumption.txt"
+
+# Download and unzip the data file into the current working directory
+downloadData <- function() {
+  zipFileName = "pc.zip"
+  fileURL = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  
+  download.file(fileURL, zipFileName, method="curl")
+  unzip(zipFileName)
+}
+
+# Load the data from the data file for the required time period
+# and convert column classes as needed for plotting
+loadData <- function(name = "household_power_consumption.txt") {
+  allData <- read.csv(fileName, sep=";", stringsAsFactors=FALSE)
+  allData$Date <- as.Date(allData$Date, format="%d/%m/%Y")
+  data <- allData[allData$Date >= as.Date("2007-02-01") & allData$Date <= as.Date("2007-02-02"),]
+  data$Global_active_power <- as.double(data$Global_active_power)
+  
+  data
+}
+
+plot4 <- function(frame) {
+  png("plot4.png")
+  
+}
+
+downloadData()
+frame <- loadData()
+plot4(frame$Global_active_power)
