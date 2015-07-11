55fa4e2a78cb3b8ead9c34f1ec2bb74c5a7065f3
Cleveland, Brennan <Brennan.Cleveland@bestbuy.com>
2015-07-11 12:37:46 -0500

    Add plot source file

---
+++ plot1.R
@@ -0,0 +1,50 @@
+
+fileName = "household_power_consumption.txt"
+
+# Download and unzip the data file into the current working directory
+downloadData <- function() {
+    zipFileName = "pc.zip"
+    fileURL = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+    download.file(fileURL, zipFileName, method="curl")
+    unzip(zipFileName)
+}
+
+# Load the data from the data file for the required time period
+# and convert column classes as needed for plotting
+loadData <- function(name = "household_power_consumption.txt") {
+    allData <- read.csv(fileName, sep=";", stringsAsFactors=FALSE)
+    allData$Date <- as.Date(allData$Date, format="%d/%m/%Y")
+    data <- allData[allData$Date >= as.Date("2007-02-01") & allData$Date <= as.Date("2007-02-02"),]
+    data$Global_active_power <- as.double(data$Global_active_power)
+    
+    data
+}
+
+plot1 <- function(frame) {
+    png("plot1.png")
+    par(bg = "white")
+    par(fg = "black")
+    par(cex = 0.75)
+    hist(frame, ylim=c(0,1300), col = "red", main = "Global Active Power", xlab="Global Active Power (killowatts)", ylab="Frequency")    
+    dev.off()
+}
+
+plot2 <- function(frame) {
+    png("plot2.png")
+    par(bg = "white")
+    par(fg = "black")
+    #par(cex = 0.75)
+    plot(frame, type="l", xlab="", ylab="Global Active Power (killowatts)", xaxt="n")
+    axis(1, at=c(0, 1440, 2880), labels=c("Thu", "Fri", "Sat"))
+    dev.off()
+}
+
+plot3 <- function(frame) {
+    
+}
+
+#downloadData()
+#frame <- loadData()
+#plot1(frame$Global_active_power)
+#plot2(frame$Global_active_power)
